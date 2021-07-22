a442f4a790ab5fbce10951eeb5ddb1f37d99f32c
marciaDelCastillo <marcia990_14@hotmail.com>
2021-07-21 21:13:36 -0300

    vistas dinamicas y uso de archivos json

---
+++ src/controllers/mainController.js
@@ -0,0 +1,13 @@
+const platos = require("../data/platosdb");
+
+module.exports={
+    index: (req,res)=> {
+        return res.render('index',{platos,});
+    },
+    detail:(req,res)=> {
+        let plato = platos.find(plato=> plato.id === +req.params.id);
+        return res.render("detalleMenu", {plato,});
+    }
+        
+        
+}
---
+++ src/data/platosdb.js
@@ -0,0 +1,3 @@
+const fs =require("fs");
+const path = require("path");
+module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, "platos.json"), "utf-8"));
